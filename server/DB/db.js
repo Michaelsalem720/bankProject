45ddc133a4e94a8615dd9eb8f05cd3f9f3b85874
eyal <eyalalon2000@gmail.com>
2023-01-29 15:06:00 -0500

    v4

---
+++ server/DB/db.js
@@ -0,0 +1,49 @@
+var mysql = require('mysql');
+let con = require('./con')
+const fs = require ('fs')
+
+// con.connect(function (err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+//   con.query("CREATE DATABASE a", function (err, result) {
+//     if (err) throw err;
+//     console.log("Database created");
+//   });
+// });
+
+
+
+function createTable(jsonFile) {
+  let jsonArr = []
+  for (let obj of jsonFile.columns) {
+    let newArr = []
+    for (key in obj) {
+      newArr.push(obj[key])
+    }
+    jsonArr.push(newArr.join(' '))
+  }
+  return jsonArr.join(', ')
+}
+function createT(jsonFile) {
+  let tableName = jsonFile.table
+  let sql = `CREATE TABLE ${tableName} (${createTable(jsonFile)})`
+  con.query(sql, function (err, db) {
+    if (err) {
+      throw err
+    }
+    console.log("Created", db)
+  })
+}
+
+fs.readdir("./DB/jsons", function (err, files) {
+  if (err) throw err
+  files.forEach(file => {
+    const path = `/home/hilma/bankProject/server/DB/jsons/${file}`
+    console.log(path)
+    fs.readFile(path, "utf-8", function (err, data) {
+      if (err) throw err
+      createT(JSON.parse(data))
+    })
+  })
+})
+
