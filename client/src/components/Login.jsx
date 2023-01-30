1c152facd661b445b3c9adba66c36346c23434c9
Michael <Michaelsalem720@gmail.com>
2023-01-30 10:25:48 +0200

    v4

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import UserContext from "../context/userContext";
 import Home from "./Home";
 // function Login() {
@@ -11,24 +11,37 @@ import Home from "./Home";
 
 
 function Login() {
+
+    const [loginInfo, setLoginInfo] = useState({ username: "", password: "" });
+
     const { setId } = useContext(UserContext);
 
-    const handleLogin = (e,id) => {
+    function handleChange(e) {
+        let { name, value } = e.target;
+        setLoginInfo({ ...loginInfo, [name]: value })
+    }
+
+    async function handleSubmit(e) {
         e.preventDefault();
-        setId(id);
-        console.log(id);
-    };
-    // function handleLogin(e,id) {
-    //     e.preventDefault();
-    //     setId(id)
-    //     console.log(id);
-    // }
+        // let res = await fetch(`http://localhost:8080/`)
+        // let data = await res.json();
+    }
+
     return (
-        <div>
-            <button onClick={(e) => handleLogin(e,1)}>Login as User 1</button>
-            <button onClick={(e) => handleLogin(e,2)}>Login as User 2</button>
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor="username">{'Username: '}
+                    <input type="text" name="username" value={loginInfo.username} onChange={handleChange} />
+                </label>
+            </div>
+            <div>
+                <label htmlFor="password">{'Password: '}
+                    <input type="text" name="password" value={loginInfo.password} onChange={handleChange} />
+                </label>
+            </div>
+            <button type="submit">Login</button>
             <Home />
-        </div>
+        </form>
     );
 }
 
--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,14 +1,136 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import ReactDOM from "react-dom";
-// import {  } from "react";
+import UserContext from "../context/userContext";
 
 function Register() {
-    return (
-        <div>
-            <h1>Register</h1>
-        </div>
-    )
-}
+    const [userInfo, setUserInfo] = useState({ firstName: '', lastName: '', username: '', password: '', passwordValidate: '', email: '', phone: '', securityQ1: '', securityA1: '', securityQ2: '', securityA2: '' });
 
+    // const { setId } = useContext(UserContext);
+
+    function handleChange(e) {
+        let { name, value } = e.target;
+        setUserInfo({ ...registrationInfo, [name]: value })
+    }
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        console.log('hello');
+        // let res = await fetch(`http://localhost:8080/`)
+        // let data = await res.json();
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label> First Name:
+                <input
+                    type="text"
+                    name="firstName"
+                    value={registrationInfo.firstName}
+                    onChange={handleChange}
+                />
+            </label>
+            <br />
+            <label>
+                Last Name:
+                <input
+                    type="text"
+                    name="lastName"
+                    value={registrationInfo.lastName}
+                    onChange={handleChange}
+                />
+            </label>
+            <br />
+            <label>
+                Username:
+                <input
+                    type="text"
+                    name="username"
+                    value={registrationInfo.username}
+                    onChange={handleChange}
+                />
+            </label>
+            <br />
+            <label>
+                Password:
+                <input
+                    type="password"
+                    name="password"
+                    value={registrationInfo.password}
+                    onChange={handleChange}
+                />
+            </label>
+            <br />
+            <label>
+                Confirm Password:
+                <input
+                    type="password"
+                    name="passwordValidate"
+                    value={registrationInfo.passwordValidate}
+                    onChange={handleChange}
+                />
+            </label>
+            <br />
+            <label>
+                Email:
+                <input
+                    type="email"
+                    name="email"
+                    value={registrationInfo.email}
+                    onChange={handleChange}
+                />
+            </label>
+            <br />
+            <label>
+                Phone Number:
+                <input
+                    type="tel"
+                    name="phone"
+                    value={registrationInfo.phone}
+                    onChange={handleChange}
+                />
+            </label>
+            <br />
+            <label>
+                Security Question 1:
+                <input
+                    type="text"
+                    name="securityQ1"
+                    value={registrationInfo.securityQ1}
+                    onChange={handleChange}
+                />
+            </label>
+            <br />
+            <label>
+                Security Answer 1:
+                <input
+                    type="text"
+                    name="securityA1"
+                    value={registrationInfo.securityA1}
+                    onChange={handleChange}
+                />
+            </label>
+            <br />
+            <label>
+                Security Question 2:
+                <input
+                    type="text"
+                    name="securityQ2"
+                    value={registrationInfo.securityQ2}
+                />
+            </label>
+            <br />
+            <label>
+                Security Answer 2:
+                <input
+                    type="text"
+                    name="securityA2"
+                    value={registrationInfo.securityA2}
+                    onChange={handleChange}
+                />
+            </label>
+            <br />
+        </form>
+    );
+};
 
 export default Register;
